fff179354f58cd2bbdd69d64a0996ebec1856bf3
Wesley Luiz - LinuxZorin OS <wesleydev03@gmail.com>
2026-02-27 05:25:38 -0300

    AJustando links

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app) # Isso permite que o site na Vercel fale com o servidor no Render
 
 app = Flask(__name__)
 CORS(app)
